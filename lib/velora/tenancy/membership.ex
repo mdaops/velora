0f460c9e6da41b79a7f5547ce6aad5496b6dc285
michael <michael@mabbott.dev>
2025-12-22 17:20:32 +0000

    chore: add accounts  | auth

--- lib/velora/tenancy/membership.ex
+++ lib/velora/tenancy/membership.ex
@@ -1,9 +1,14 @@
 defmodule Velora.Tenancy.Membership do
   use Ecto.Schema
 
-  @primary_key false
-  schema "memberships" do
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "tenant_memberships" do
+    field :role, Ecto.Enum, values: [:admin, :user]
+
     belongs_to :tenant, Velora.Tenancy.Tenant
+    belongs_to :user, Velora.Accounts.User
+
     timestamps()
   end
 end
--- lib/velora/tenancy/tenant.ex
+++ lib/velora/tenancy/tenant.ex
@@ -2,11 +2,13 @@ defmodule Velora.Tenancy.Tenant do
   use Ecto.Schema
   @moduledoc false
 
-  @primary_key false
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "tenants" do
-    field :id, :binary_id
     field :name, :string
     field :slug, :string
+
+    has_many :memberships, Velora.Tenancy.Membership
 
     timestamps()
   end
